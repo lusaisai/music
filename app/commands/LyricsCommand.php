01255bfecb441874a7456e866134f791fe3e7c68
lusaisai <lusaisai@163.com>
2014-05-06 02:17:17 +0800

    add artisan lyric command

---
+++ app/commands/LyricsCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class LyricsCommand extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'admin:lyrics';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Search and update song lyrics';
+
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function fire()
+	{
+		$path = app_path();
+		$jar = $path .'/LyricSearch/target/LyricSearch-1.0-SNAPSHOT-jar-with-dependencies.jar';
+		system( "java -jar $jar");
+	}
+
+}
