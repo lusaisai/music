332dc3c7a453d8c338efb3f6fe4e973acdb6381f
lusaisai <lusaisai@163.com>
2015-03-16 16:01:07 +0800

    modify fcgi command

--- app/commands/start-php-fcgi.bat
+++ app/commands/start-php-fcgi.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 ECHO Starting PHP FastCGI...
-set PATH=C:\webtools\php-5.6.2-Win32-VC11-x86;%PATH%
-C:\webtools\php-5.6.2-Win32-VC11-x86\RunHiddenConsole.exe C:\webtools\php-5.6.2-Win32-VC11-x86\php-cgi.exe -b 127.0.0.1:9000
+set PATH=C:\webserver\php-5.6.6-nts-Win32-VC11-x86;%PATH%
+C:\webserver\php-5.6.6-nts-Win32-VC11-x86\RunHiddenConsole.exe C:\webserver\php-5.6.6-nts-Win32-VC11-x86\php-cgi.exe -b 127.0.0.1:9000
